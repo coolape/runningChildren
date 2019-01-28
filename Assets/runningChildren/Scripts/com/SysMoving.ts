0695798edfdbc43218d32f107ae52d5d17917b47
chenbin <chbin12@126.com>
2019-01-28 23:03:23 +0800

    moving script

---
+++ Assets/runningChildren/Scripts/com/SysMoving.ts
@@ -0,0 +1,20 @@
+
+namespace game {
+
+  @ut.executeAfter(ut.Shared.UserCodeStart)
+  @ut.executeBefore(ut.Shared.UserCodeEnd)
+    /** New System */
+    export class SysMoving extends ut.ComponentSystem {
+        
+        OnUpdate():void {
+            this.world.forEach([ut.Core2D.TransformLocalPosition, game.CPMoving], (transform, moving)=>
+            {
+                let pos = transform.position;
+                pos.x += moving.speed.x*0.01;
+                pos.y += moving.speed.y*0.01;
+                pos.z += moving.speed.z*0.01;
+                transform.position = pos;
+            });
+        }
+    }
+}
---
+++ Assets/runningChildren/Scripts/com/SysMovingReset.ts
@@ -0,0 +1,30 @@
+
+namespace game {
+
+  @ut.executeAfter(ut.Shared.UserCodeStart)
+  @ut.executeBefore(ut.Shared.UserCodeEnd)
+    /** New System */
+    export class SysMovingReset extends ut.ComponentSystem {
+        
+        OnUpdate():void {
+            this.world.forEach([ut.Core2D.TransformLocalPosition, game.CPMovingReset], (transform, movingReset)=>
+            {
+                let pos = transform.position;
+                if (movingReset.axis.toLowerCase() == "x") {
+                    if (pos.x >= movingReset.threshold) {
+                        pos.x += movingReset.distance;
+                    }
+                } else if (movingReset.axis.toLowerCase() == "y") {
+                    if (pos.y >= movingReset.threshold) {
+                        pos.y += movingReset.distance;
+                    }
+                } else {
+                    if (pos.z >= movingReset.threshold) {
+                        pos.z += movingReset.distance;
+                    }
+                }
+                transform.position = pos;
+            });
+        }
+    }
+}
